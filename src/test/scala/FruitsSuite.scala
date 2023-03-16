9b585df73c063c348752fedbc4f3dea50c25fc31
olgamaluhacodacy <olga.maluha@codacy.com>
2023-03-16 11:07:03 +0100

    add Fruits 4

--- src/test/scala/FruitsSuite.scala
+++ src/test/scala/FruitsSuite.scala
@@ -4,8 +4,12 @@ import org.scalatest.matchers.must.Matchers
 
 class FruitsSuite extends AnyFlatSpec with Matchers  {
 
-  "one, two, three" should "return apple" in {
+  "one, two, three" should "return apple and banana" in {
     assert(Fruits.fruits(List(1, 2)) == List("apple","banana"))
+  }
+
+  "three" should "return peach" in {
+    assert(Fruits.fruits(List(3)) == List("peach"))
   }
 
 }
