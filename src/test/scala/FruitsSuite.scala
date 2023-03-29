6e825b5e552232b675c7263f384c10e0264c9dcb
olgamaluhacodacy <olga.maluha@codacy.com>
2023-03-29 15:14:54 +0200

    3commit2

--- src/test/scala/FruitsSuite.scala
+++ src/test/scala/FruitsSuite.scala
@@ -7,10 +7,10 @@ class FruitsSuite extends AnyFlatSpec with Matchers  {
   "one, two, three" should "return apple and banana" in {
     assert(Fruits.fruits(List(1, 1)) == List("apple","apple"))
   }
-//
-//  "three" should "return peach" in {
-//    assert(Fruits.fruits(List(3)) == List("peach"))
-//  }
+
+  "three" should "return peach" in {
+    assert(Fruits.fruits(List(3)) == List("peach"))
+  }
 //
 //  "four" should "return orange" in {
 //    assert(Fruits.fruits(List(3)) == List("peach"))
