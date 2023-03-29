15e64cf1f0b3e5e7c86d6a6a14533110ab19f899
olgamaluhacodacy <olga.maluha@codacy.com>
2023-03-29 14:22:13 +0200

    2commit1

--- src/main/scala/Fruits.scala
+++ src/main/scala/Fruits.scala
@@ -4,8 +4,8 @@ object Fruits {
     list.map {
       case 1 => "apple"
       case 2 => "banana"
-      case 3 => "peach"
-      case 4 => "orange"
+//      case 3 => "peach"
+//      case 4 => "orange"
       case 5 => "grapes"
       case _ => "unknown fruit"
     }
