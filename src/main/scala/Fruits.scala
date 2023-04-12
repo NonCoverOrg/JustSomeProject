47c1a7ee85a6dc53218e600938672810d2a159ea
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-12 15:38:02 +0200

    9commit1

--- src/main/scala/Fruits.scala
+++ src/main/scala/Fruits.scala
@@ -1,6 +1,29 @@
+import String.valueOf
 object Fruits {
 
   def fruits(list: List[Int]): List[String] = {
+    list.map {
+      case 1 => "apple"
+      case 2 => "banana"
+      case 3 => "peach"
+//      case 4 => "orange"
+//      case 5 => "grapes"
+      case _ => "unknown fruit"
+    }
+  }
+
+  def fruits222(list: List[Int]): List[String] = {
+    list.map {
+      case 1 => "apple"
+      case 2 => "banana"
+      case 3 => "peach"
+//      case 4 => "orange"
+//      case 5 => "grapes"
+      case _ => "unknown fruit"
+    }
+  }
+
+  def fruits222333(list: List[Int]): List[String] = {
     list.map {
       case 1 => "apple"
       case 2 => "banana"
