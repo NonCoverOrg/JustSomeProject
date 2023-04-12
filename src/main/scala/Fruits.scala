8188855af1087bafd5be6783962f9cbdc9164d3e
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-12 15:16:52 +0200

    8commit1

--- src/main/scala/Fruits.scala
+++ src/main/scala/Fruits.scala
@@ -5,8 +5,8 @@ object Fruits {
       case 1 => "apple"
       case 2 => "banana"
       case 3 => "peach"
-      case 4 => "orange"
-      case 5 => "grapes"
+//      case 4 => "orange"
+//      case 5 => "grapes"
       case _ => "unknown fruit"
     }
   }
