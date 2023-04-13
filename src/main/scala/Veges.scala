34ece61875c8f153c530ae38f899e21ce9e645f1
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-13 11:29:36 +0200

    222222

--- src/main/scala/Veges.scala
+++ src/main/scala/Veges.scala
@@ -2,4 +2,6 @@ class Veges {
 
   def printCucumber(): Unit = println("Cucumber")
 
+  def printCucumber11(): Unit = println("Cucumber")
+
 }
--- src/test/scala/RomanNumeralsSuite.scala
+++ src/test/scala/RomanNumeralsSuite.scala
@@ -8,9 +8,9 @@ class RomanNumeralsSuite extends AnyFlatSpec with Matchers {
 //    assert(rom.fromRoman("MDCLXVI") == 1666)
 //  }
 
-  "toRoman" should "return correct number" in {
-    val rom = new RomanNumerals
-    assert(rom.toRoman(1666) == "MDCLXVI")
-  }
+//  "toRoman" should "return correct number" in {
+//    val rom = new RomanNumerals
+//    assert(rom.toRoman(1666) == "MDCLXVI")
+//  }
 
 }
