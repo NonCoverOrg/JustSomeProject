4daff20ade5f5b53fc31663b051a12709cc8a012
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-13 16:42:25 +0200

    11

--- src/main/scala/Veges.scala
+++ src/main/scala/Veges.scala
@@ -1,5 +1,12 @@
 class Veges {
 
-  def printCucumber(): Unit = println("Cucumber")
+  def printCucumber(): Unit = {
+    println("Cucumber")
+    println("Cucumber")
+    println("Cucumber")
+    println("Cucumber")
+    println("Cucumber")
+    println("Cucumber")
+  }
 
 }
