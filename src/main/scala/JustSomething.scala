c67e259ed76dd41986c0ea6e97169c5d1357cb6d
olgamaluhacodacy <olga.maluha@codacy.com>
2023-04-18 12:41:25 +0200

    1

--- src/main/scala/JustSomething.scala
+++ src/main/scala/JustSomething.scala
@@ -3,25 +3,6 @@ class JustSomething {
   def sayHi(): Unit = {
     println("HI there")
     println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
-    println("HI there")
   }
 
 }
